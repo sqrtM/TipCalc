0eef2cde38eef6e36c759c455eb808368c3477d8
mason <mpike21797@gmail.com>
2022-11-03 15:48:07 +0100

    added tip stuff

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -3,17 +3,19 @@ package com.example.tiptime
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,22 +46,39 @@ private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
 }
 
 @Composable
-fun EditNumberField(value: String, onValueChange: (String) -> Unit) {
+fun EditNumberField(@StringRes label: Int, value: String, onValueChange: (String) -> Unit, ime: ImeAction, modifier: Modifier = Modifier) {
+    val focusManager = LocalFocusManager.current
     TextField(
         value = value,
         onValueChange = onValueChange,
-        label = { Text(stringResource(id = R.string.cost_of_service)) },
+        label = { Text(stringResource(label)) },
         modifier = Modifier.fillMaxWidth(),
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number, imeAction = ime),
+        keyboardActions = KeyboardActions(onNext = {focusManager.moveFocus(FocusDirection.Down)}, onDone = {focusManager.clearFocus()})
     )
+}
+
+@Composable
+fun RoundTheTipRow(roundUp: Boolean, onRoundUpChanged: (Boolean) -> Unit, modifier: Modifier = Modifier) {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .size(48.dp), verticalAlignment = Alignment.CenterVertically) {
+        Text(text = stringResource(id = R.string.round_up_tip))
+        Switch(modifier = modifier
+            .fillMaxWidth()
+            .wrapContentWidth(), checked = roundUp, onCheckedChange = onRoundUpChanged)
+    }
 }
 
 @Composable
 fun TipTimeScreen() {
     var amountInput by remember { mutableStateOf("") }
+    var tipInput by remember { mutableStateOf("") }
     val amount = amountInput.toDoubleOrNull() ?: 0.0
-    val tip = calculateTip(amount)
+    val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount, tipPercent)
+    var roundUp by remember { mutableStateOf(false) }
     Column(
         modifier = Modifier.padding(32.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
@@ -70,8 +89,11 @@ fun TipTimeScreen() {
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
         Spacer(Modifier.height(16.dp))
-        EditNumberField(value = amountInput, onValueChange = { amountInput = it })
+        EditNumberField(label = R.string.bill_amount, value = amountInput, onValueChange = { amountInput = it }, ime = ImeAction.Next)
         Spacer(Modifier.height(24.dp))
+        EditNumberField(label = R.string.how_was_the_service, value = tipInput, onValueChange = { tipInput = it }, ime = ImeAction.Done)
+        Spacer(Modifier.height(24.dp))
+        RoundTheTipRow(roundUp = roundUp, onRoundUpChanged = { roundUp = it })
         Text(
             text = stringResource(id = R.string.tip_amount, tip),
             modifier = Modifier.align(Alignment.CenterHorizontally),
